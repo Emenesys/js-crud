c2aee8f93001f6baf9783b0fb957627dd4c71311
CLEBER JOSE COSTA <48730510+Emenesys@users.noreply.github.com>
2023-04-18 17:47:37 -0300

    atualização de promisse e sincronismo
    atualização

--- projeto_inicial/admin/service/cliente-service.js
+++ projeto_inicial/admin/service/cliente-service.js
@@ -1,47 +1,11 @@
 
-const criaNovaLinha = (nome, email) => {
-    const linhaNovoCliente = document.createElement('tr')
-    const conteudo = `
-    <td class="td" data-td>${nome}</td>
-                <td>${email}</td>
-                <td>
-                    <ul class="tabela__botoes-controle">
-                        <li><a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a></li>
-                        <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
-                    </ul>
-                </td> -->
-                `
-    linhaNovoCliente.innerHTML = conteudo
-    return linhaNovoCliente
+export const listaClientes = () => {  
+    return fetch(`Http://localhost:3000/profile`)
+    .then(resposta => {
+        return resposta.json()
+    })  
+} 
+export const clienteService = {
+    listaClientes
+
 }
-
-const tabela = document.querySelector('[data-tabela]')
-
-
-const listaClientes = () => {
-    
-    const promise = new Promise ((resolve, reject) => {
-        const http = new XMLHttpRequest()
-
-        http.open('GET', 'http://localhost:3000/profile')
-
-        http.send()
-
-        http.onload = () => {
-            if(http.status >= 400) {
-                reject (JSON.parse(http.response))
-            }else {
-                resolve (JSON.parse(http.response))
-            }
-        }
-        http.send()
-    })
-        console.log(promise)                
-        return promise    
-}
-
-listaClientes()
-
-const data = JSON.parse(http.response)
-    data.forEach(elemento => {
-        tabela.appendChild(criaNovaLinha(elemento.nome, elemento.email))})
