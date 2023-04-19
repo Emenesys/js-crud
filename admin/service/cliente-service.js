3a3db206b4c60491ba4e8fab475d6407a04c2565
CLEBER JOSE COSTA <48730510+Emenesys@users.noreply.github.com>
2023-04-19 14:19:00 -0300

    copia do arquivo original da alura
    correção com arquivo novo

--- admin/service/cliente-service.js
+++ admin/service/cliente-service.js
@@ -1,30 +1,34 @@
-
-const listaClientes = () => {  
-    return fetch(`Http://localhost:3000/profile`)
+const listaClientes = () =>  {
+    return fetch(`http://localhost:3000/profile`)
     .then(resposta => {
         return resposta.json()
-    })  
-} 
-const criaCliente = (nome, email) => {
-    return fetch(`Http://localhost:3000/profile`, {
-        method: 'POST',
+    })
+}
+
+const criaCliente = (nome, email) => { 
+    return fetch(`http://localhost:3000/profile`, {
+        method: 'POST', 
         headers: {
             'Content-Type' : 'application/json'
         },
-        body: JSON.stringify ({
+        body: JSON.stringify({
             nome: nome,
             email: email
         })
-       })
-       .then( resposta => {
+    })
+    .then( resposta => {
         return resposta.body
-       })
-    
+    })
 }
 
+const removeCliente = (id) => { 
+    return fetch(`http://localhost:3000/profile/${id}`, {
+        method: 'DELETE'
+    })
+}
 
-export const clienteService = {
+export const clienteService = { 
     listaClientes,
-    criaCliente
-
+    criaCliente, 
+    removeCliente
 }
