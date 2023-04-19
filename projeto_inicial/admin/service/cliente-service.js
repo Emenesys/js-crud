96164ec3b6aef3d207f256730caa31c30de37106
CLEBER JOSE COSTA <48730510+Emenesys@users.noreply.github.com>
2023-04-19 13:07:34 -0300

    atualização do cadastro de clientes
    atualização

--- projeto_inicial/admin/service/cliente-service.js
+++ projeto_inicial/admin/service/cliente-service.js
@@ -1,11 +1,30 @@
 
-export const listaClientes = () => {  
+const listaClientes = () => {  
     return fetch(`Http://localhost:3000/profile`)
     .then(resposta => {
         return resposta.json()
     })  
 } 
+const criaCliente = (nome, email) => {
+    return fetch(`Http://localhost:3000/profile`, {
+        method: 'POST',
+        headers: {
+            'Content-Type' : 'application/json'
+        },
+        body: JSON.stringify ({
+            nome: nome,
+            email: email
+        })
+       })
+       .then( resposta => {
+        return resposta.body
+       })
+    
+}
+
+
 export const clienteService = {
-    listaClientes
+    listaClientes,
+    criaCliente
 
 }
